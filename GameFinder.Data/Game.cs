91d3a9b2c7fa2daba09550e4f364802fa4b6abe6
amoyer-blip <75403219+amoyer-blip@users.noreply.github.com>
2021-04-15 12:45:20 -0400

    Updates

---
+++ GameFinder.Data/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFinder.Data
+{
+    public enum GamingConsole { Xbox = 1, PlayStation, Nintendo, Sega}
+    public class Game
+    {
+        [Key]
+        public int GameId { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public bool FamilyFriendly { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+    }
+}
